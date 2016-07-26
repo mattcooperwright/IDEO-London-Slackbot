2e542b251e0ac5b1f425305a8d136aba99f540a5
Matt Cooper-Wright <mattcooperwright@ideo.com>
2016-07-26 15:43:24 -0700

    Tidying up old code
    Pulling some less useful features to leave just the vital stuff

--- main.js
+++ main.js
@@ -1,67 +1,18 @@
 Parse.Cloud.define("zendrive_webhook", function(request, response) {
     
     var driver = request.params.driver_id;
-    var trip = request.params.trip_id;
-    var tripString = '' + trip;
-    var rando = Math.floor((Math.random() * 3) + 1);
     var pushMessage = "";
     var messageArray = ["Oh look :eyes: , a new trip by",
                         ":newspaper: Good news, another trip logged by",
                         "if you're interested :mag: , we've just spotted another drive by",
                         "would you believe it :car: , a new drive by",
                         "Oh, a drive just got added to the database :computer:. It was by"];
-    
-    console.log("Starting logger");
-    var tripLogClass = Parse.Object.extend("tt");
-    var tripLog = new tripLogClass();
-
-    tripLog.set('tripID', tripString);
-    tripLog.set('fromWebhook', true);
-
-    tripLog.save(null, {
-        success: function(atripLog) {
-            console.log("Successfully saved a trip");
-            response.success();
-        },
-
-        error: function(pointAward, error) {
-            console.log("Could not save a trip " + error.message);
-            response.error(error);
-        }
-
     });
-    
-    /*Push*/ 
-    var query = new Parse.Query(Parse.Installation);
-    query.equalTo('username', driver);
-     
-    if (rando == 200 ) {
-
-        Parse.Push.send({
-            where: query,
-            data: {
-                alert: "Drive recorded, please complete a quick post drive survey",
-                tripID: trip
-          }
-        }, { success: function() { 
-            alert("Post trip notification sent to:" + driver + 'code:' + rando);
-            push = "A push notification was sent";
-          }, 
-            error: function(error) { 
-            alert("Could not send push " + error);
-            }
-        });
-        
-        pushMessage = "sent notification";
-        
-    };
-
-    
     
     /* Slack */
     Parse.Cloud.httpRequest({
         method: 'POST',
-        url: 'https://hooks.slack.com/services/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',
+        url: 'ADD_YOUR_INCOMING_SLACK_URI_HERE',
         headers: {
             'Content-Type': 'application/json'
         },
